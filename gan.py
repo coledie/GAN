0420e618ad4d474a7be6a8b1ab1ced3a0ed60c1a
csdhv9 <csdhv9@umsystem.edu>
2020-10-18 10:54:37 -0500

    Add params.

--- gan.py
+++ gan.py
@@ -3,6 +3,8 @@ GAN made with pytorch based on the paper,
 
 Generative Adversarial Nets.
 Goodfellow, Abadie, et al.
+
+Params: https://github.com/goodfeli/adversarial/blob/master/mnist.yaml
 """
 """ Experiment Design
 Adversarial modelling framework - 2 models.
@@ -84,12 +86,14 @@ class Generator(nn.Module):
     """
     Generate random samples.
 
-    Mix of rectifier linear and sigmoid activations.
+    Rectified linear, sigmoid output activations.
     Input: Noise
     Output: Generative sample
     """
-    def __init__(self):
-        pass
+    def __init__(self, layers):
+        # all but output should be rectified linear
+        # output = sigmoid
+        # irange = .05
 
     def forward(self, z) -> torch.Tensor:
         pass
@@ -99,14 +103,17 @@ class Discriminator(nn.Module):
     """
     Determine whether sample is real or generated.
 
-    Maxout activations.
+    Maxout, sigmoid output activations.
     Dropout.
 
     Input: Image
     Output: Classification - Generated or real.
     """
-    def __init__(self):
-        pass
+    def __init__(self, layers):
+        # main maxout activation, output=sigmoid
+        # irange = .005
+
+        # No dropout for mnist
 
     def forward(self, x) -> int:
         pass
@@ -115,32 +122,37 @@ class Discriminator(nn.Module):
 if __name__ == '__main__':
     N_EPOCH = 10
     DISCRIMINATOR_STEPS = 1
-    BATCH_SIZE = 4  # TODO what is m for mnist?
+    BATCH_SIZE = 100
 
-    train_set = MNIST('mnist_train.csv', batch_size=BATCH_SIZE)
+    train_set = MNIST('mnist_train.csv', batch_size=BATCH_SIZE)  # NOTE 10k of mnist
 
-    print(train_set.sample(), len(train_set.sample()[0]))
-    ## TODO Init models
+    generator = Generator((1200, 1200, 784))
+    discriminator = Discriminator((240, 240, 1))
 
     gen_criterion = nn.CrossEntropyLoss()  # TODO replace with Generator / Discriminator Loss
     disc_criterion = nn.CrossEntropyLoss()  # TODO replace with Generator / Discriminator Loss
     
-    # TODO momentum for both?
-    gen_optimizer = torch.optim.SGD(generator.parameters(), lr=0.0001, momentum=0)
-    disc_optimizer = torch.optim.SGD(discriminator.parameters(), lr=0.0001, momentum=0)
+    # TODO lr start=.1, decay_factor=1.000004, min=.000001; mo start=.5, stop=.7.
+    gen_optimizer = torch.optim.SGD(generator.parameters(), lr=0.1, momentum=0.5)  
+    disc_optimizer = torch.optim.SGD(discriminator.parameters(), lr=0.1, momentum=0.5)
 
     for epoch in range(N_EPOCH):
         for k in range(DISCRIMINATOR_STEPS):
             disc_optimizer.zero_grad()
 
-            z = p_g(z, size=BATCH_SIZE)
-            x = train_set.sample()[0]  # p_data()
+            z = noise(size=BATCH_SIZE)  # TODO what is dist of noise?
+            x_gen = p_g(z)
 
-            #discriminator.backward() = ascending grad(theta_d) 1/m sum_i in m(log(d(x_i)) + log(1 - D(G(z_i)))).
+            x_real = train_set.sample()[0]  # p_data()
+
+            # TODO
+            #discriminator.backward() = ascending grad(theta_d) 1/m sum_i in m(log(d(x_real_i)) + log(1 - D(G(z_i)))).
 
         gen_optimizer.zero_grad()
 
-        z = p_g(z, size=BATCH_SIZE)
+        z = noise(size=BATCH_SIZE)  # TODO what is dist of noise?
+        x_gen = p_g(z, size=BATCH_SIZE)
+        # TODO
         #generator.backward() = descending grad(theta_g) 1 / m sum_i in m(log(1 - D(G(z_i))))
 
-    ## TODO generate some test samples
+    ## TODO vis generated images
